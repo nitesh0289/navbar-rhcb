fc99afa53a2ae725810b04bd18e4c40062fcd6ed
A-phenomenal1 <niteshch200053@gmail.com>
2022-06-14 17:12:45 +0530

    initial commit

--- app.js
+++ app.js
@@ -15,12 +15,10 @@ const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
 
 navToggle.addEventListener("click", function () {
-  //   console.log(links.classList.contains("show-links"));
-  //   if (links.classList.contains("show-links")) {
-  //     links.classList.remove("show-links");
-  //   } else {
-  //     links.classList.add("show-links");
-  //   }
-
-  links.classList.toggle("show-links");
+  console.log(links.classList.contains("show-links"));
+  if (links.classList.contains("show-links")) {
+    links.classList.remove("show-links");
+  } else {
+    links.classList.add("show-links");
+  }
 });
